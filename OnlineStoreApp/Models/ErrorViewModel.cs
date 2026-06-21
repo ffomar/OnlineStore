510637ecb2e6c29509e2d6034c0ea1fd727823d4
farouokq <farook.omar10@yahoo.com>
2026-06-21 17:36:43 -0500

    Initial commit

---
+++ OnlineStoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OnlineStoreApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
