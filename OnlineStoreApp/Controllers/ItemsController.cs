4d5bcdf0c41aa095f96329611e0ac2164432f976
farouokq <farook.omar10@yahoo.com>
2026-06-24 20:14:27 -0500

    the project's first MVC

---
+++ OnlineStoreApp/Controllers/ItemsController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace OnlineStoreApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+
+        public IActionResult Overview()
+        {   
+            var item = new Item() {Name = "keyboard"};
+            return View(item);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id = " + id);        
+        }
+    }
+}
